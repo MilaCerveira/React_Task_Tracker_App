14b79f0873290973fd91a7dbf8a10df04a0d8329
milacerveira <milascerveira@gmail.com>
2021-03-28 20:14:51 +0100

    delete

--- src/App.js
+++ src/App.js
@@ -18,9 +18,8 @@ const App = () => {
    ])
 
    //Delete Task 
-
    const deleteTask= (id) => {
-     console.log('delete', id);
+     setTasks(tasks.filter((task) => task.id !== id))
    }
   
   return (
