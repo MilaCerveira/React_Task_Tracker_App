1cca301d5f21f936aac41d010a006907eda2e3f9
milacerveira <milascerveira@gmail.com>
2021-03-28 17:25:51 +0100

    objects with id  & text reminder

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,3 +1,11 @@
+const Tasks = [
+ {id: 1,
+    text: 'Walk the dog so he stops chewing the furniture',
+    day: 'March 28th at 19.00pm',
+    reminder: true,
+}, 
+
+]
 
 const Tasks = () => {
     return (
