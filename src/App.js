16879adeaec10f920c0982e7608c281fd62aeb70
milacerveira <milascerveira@gmail.com>
2021-03-28 20:06:56 +0100

    delete

--- src/App.js
+++ src/App.js
@@ -16,11 +16,17 @@ const App = () => {
    }, 
    
    ])
+
+   //Delete Task 
+
+   const deleteTask= (id) => {
+     console.log('delete', id);
+   }
   
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={tasks}/>
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { FaTrash } from 'react-icons/fa'
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
     return (
         <div className='task'>
-            <h3>{task.text} <FaTrash style={{ color: '#A71D31', cursor:'pointer' }}/>
+            <h3>{task.text} <FaTrash style={{ color: '#A71D31', cursor:'pointer' }} onClick={() => onDelete(task.id)}/>
             </h3>
             <p>{task.day}</p>
             
